b40f6b11b084fcd64fa154c5ab0ed170ee98c66c
Chris <lars4647@umn.edu>
2013-09-18 20:51:12 -0500

    few changes to bounty

--- src/Commands/CheckBounties.java
+++ src/Commands/CheckBounties.java
@@ -7,6 +7,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.earth2me.essentials.Essentials;
 import com.earth2me.essentials.User;
@@ -23,7 +24,16 @@ public class CheckBounties implements CommandExecutor{
 	@Override
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2,
 			String[] arg3) {
-		User user = this.ess.getUser(arg0.getName());
+		User user = null;
+		if(arg3.length == 0 && arg0 instanceof Player == false){
+			arg0.sendMessage(ChatColor.RED+"That command can only be executed ingame!");
+		}else if(arg3.length == 0)
+			user = this.ess.getUser(arg0.getName());
+		else
+			user = this.ess.getOfflineUser(arg3[0]);
+		if(user == null){
+			arg0.sendMessage(ChatColor.RED+"Player not found. Did you type the name right?");
+		}
 		if(user.getConfigMap().containsKey("bounties")){
 			@SuppressWarnings("unchecked")
 			List<String> bounties = (List<String>) user.getConfigMap().get("bounties");
@@ -31,7 +41,7 @@ public class CheckBounties implements CommandExecutor{
 				arg0.sendMessage(ChatColor.GREEN+"You don't have any bounties!");
 				return false;
 			}
-			StringBuilder builder = new StringBuilder().append(ChatColor.BLUE).append("You currently have the following bounties:\n");
+			StringBuilder builder = new StringBuilder().append(ChatColor.BLUE).append("Bounties:\n");
 			for(String bounty:bounties){
 				String[] split = bounty.split("[:]");
 				String format = DateUtil.formatDateDiff(Long.parseLong(split[1]));
